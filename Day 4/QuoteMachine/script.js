42bf19dc872dc1d486f9d0fc41b128efaa8b187e
Joanna <j.puno0909@gmail.com>
2018-10-14 18:37:50 -0600

    quotemachine-sundaypush

--- Day 4/QuoteMachine/script.js
+++ Day 4/QuoteMachine/script.js
@@ -1,25 +1,45 @@
 var quotes = [
-    '"True humility is not thinking less of yourself; it is thinking of yourself less. -- C.S. Lewis"',
+    'True humility is not thinking less of yourself; it is thinking of yourself less.',
 
-    '"You know you\'re in love when you can\'t fall asleep because reality is finally better than your dreams. -- Dr. Seuss"',
+    'You know you\'re in love when you can\'t fall asleep because reality is finally better than your dreams.',
 
-    '"There can be no tyrants where there are no slaves.-- Jose Rizal"',
+    'There can be no tyrants where there are no slaves.',
 
-    '"I can\'t go back to yesterday - because I was a different person then.-- Lewis Carroll" ',
+    'I can\'t go back to yesterday - because I was a different person then.',
 
-    '"People are more difficult to work with than machines. And when you break a person, he can\'t be fixed. -- Rick Riordan"'];
+    'People are more difficult to work with than machines. And when you break a person, he can\'t be fixed.'];
+
+var quoteAuthor = [
+    'C.S. Lewis',
+    'Dr. Seuss',
+    'Jose Rizal',
+    'Lewis Carroll',
+    'Rick Riordan'
+];
+
+
+var nextImages = [
+    './img/cslewis.jpg',
+    './img/drSeuss.jpg',
+    './img/joseRizal.jpg',
+    './img/lewisCarroll.jpg',
+    './img/rickRiordan.jpg'
+];
 
 // Buttons
 
 var backButton = document.getElementById('backButton');
 var nextButton = document.getElementById('nextButton');
 
-var authorImg = document.getElementById('img-box');
 
+// Variables for Quote + Author + Author Images
 
 var counter = 0;
-
 document.getElementById('quote').innerHTML = quotes[counter];
+document.getElementById('quote-author').innerHTML = quoteAuthor[counter];
+document.getElementById('main-img').src = nextImages[counter];
+
+// Changing Quotes + Author + Author Images
 
 function nextQuote() {
     if(counter < quotes.length - 1){ 
@@ -28,7 +48,8 @@ function nextQuote() {
         counter = 0;
     }
     document.getElementById('quote').innerHTML = quotes[counter]; 
-    
+    document.getElementById('quote-author').innerHTML = quoteAuthor[counter];
+    document.getElementById('main-img').src = nextImages[counter]; 
 }
 
 function backQuote() {
@@ -38,53 +59,17 @@ function backQuote() {
         counter--;
     }
     document.getElementById('quote').innerHTML = quotes[counter];
+    document.getElementById('quote-author').innerHTML = quoteAuthor[counter];
+    document.getElementById('main-img').src = nextImages[counter];
 }
 
 // Buttons --- Events
 
-nextButton.addEventListener('click', nextQuote, nextImage);
+nextButton.addEventListener('click', nextQuote);
 backButton.addEventListener('click', backQuote);
-
 
 // Quote Timer
 setInterval(nextQuote, 3000);
-
-
-// Image Changer
-var imgArray = new Array();
-
-imgArray[0] = new Image();
-imgArray[0].src = './img/cslewis.jpg';
-
-imgArray[1] = new Image();
-imgArray[1].src = './img/drSeuss.jpg';
-
-imgArray[2] = new Image();
-imgArray[2].src = './img/joseRizal.jpg';
-
-imgArray[3] = new Image();
-imgArray[3].src = './img/lewisCarroll.jpg';
-
-imgArray[4] = new Image();
-imgArray[4].src = './img/rickRiordan.jpg';
-
-function nextImage(){
-    var img = document.getElementById('mainImage');
-
-    for(var i = 0; i < imgArray.length;i++){
-        if(imgArray[i].src == img.src){
-            if(i === imgArray.length){
-                document.getElementById('mainImage').src = imgArray[0].src;
-                
-                break;
-            }
-            document.getElementById('mainImage').src = imgArray[i+1].src;
-            break;
-        }
-    }
-}
-// Image Timer
-setInterval(nextImage, 3000);
 
 
 
@@ -93,27 +78,52 @@ setInterval(nextImage, 3000);
 
 // Animations
 
-// var direction = 0;
-// var quoteBox = document.getElementById('quote-box')
 
-//       function slideContent() {
-//         var quoteContainer = document.getElementById('quote-content');
-//         var currentPosition = parseInt(quoteContainer.style.right);
+var direction = 0;
+
+      function moveQuote() {
+        var quoteContent = document.getElementById('quote-content');
+        var quoteContainer = document.getElementById('quote-container');
+        var currentLeft = parseInt(quoteContent.style.left);
         
 
-//         if (currentPosition <= 0) {
-//           direction =+ 1;
-//         }
+        if (currentLeft > quoteContainer.width-quoteContent.width) {
+          direction = 0;
+          
+        }
 
-//         if (currentPosition > window.innerWidth - quoteContainer.width) {
-//             direction = 0;
-//           }
+        if (currentLeft <= 0) {
+          direction =+ 1;
+          
+        }
 
-//         if (direction) {
-//           quoteContainer.style.right = (currentPosition - 80) + 'px';
-//         } else {
-//           quoteContainer.style.right = (currentPosition + 80) + 'px';
-//         }
-//       }
+        if (direction) {
+          quoteContent.style.left = (currentLeft - 180) + 'px';
+        } else {
+          quoteContent.style.left = (currentLeft + 180) + 'px';
+        }
+      }
       
-//       setInterval(slideContent, 50);
+      setInterval(moveQuote, 3000);
+
+
+    //   #quote-content {
+    //     width: 60%;
+    //     position: absolute;
+    //     /* right: 130px; */
+    //     left: 180px;
+    //     /* background: red; */
+    //     top: 40px;
+    // }
+
+    // #quote-container {
+    //     width: 60%;
+    //     background: url(./img/yellowPolygon.svg);
+    //     background-size: cover;
+    //     padding-top: 10px;
+    //     position: relative;
+    //     left: 500px;
+    //     /* background: pink; */
+    //     background-repeat:no-repeat; 
+    
+    //   }
