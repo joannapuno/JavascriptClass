56195c643837790bd7d174ceb41f4c44a985314f
Joanna <j.puno0909@gmail.com>
2018-10-15 20:56:47 -0600

    Quiz-Functional

--- Day 5/Quiz/script.js
+++ Day 5/Quiz/script.js
@@ -7,23 +7,29 @@ var errorAge = document.getElementById('errorAge');
 var errorAnimal = document.getElementById('errorAnimal');
 var errorValue = document.getElementById('errorValue');
 
-submitButton.addEventListener('click', function(event){
 
-    // Current Value of Error Messages
+
+
+
+submitButton.addEventListener('click', function(event){
+// Current Value of Error Messages
     errorName.style.display = 'none';
     errorAge.style.display = 'none';
     errorAnimal.style.display = 'none';
     errorValue.style.display = 'none';
 
+ // Variables
     var name = document.quiz1['name'].value;
     var age = parseInt(document.quiz1['age'].value);
 
-    var animal = document.quiz1['favAnimal'].value;
-    var lifeValue = document.querySelectorAll('input[type="checkbox"]');
-    var checkedOne = Array.prototype.slice.call(lifeValue).some(x => x.checked);
+   
+
+    var animal = document.quiz1['favAnimal'];
+    var lifeValue = document.quiz1['important'];
+    
 
 
-    // Validation
+// Validation
     if(!name){
         event.preventDefault();
         errorName.style.display = 'block'
@@ -39,17 +45,162 @@ submitButton.addEventListener('click', function(event){
         errorAnimal.style.display = 'block'
     }
 
-    if(!checkedOne){
-
+    if(!lifeValue){
         event.preventDefault();
-        errorValue.style.display = 'block'
+        errorAnimal.style.display = 'block'
     }
+
+ // Scoring
+
+var birdFeed = 0;
+var popStar = 0;
+var drugLord = 0;
+var donDraper = 0;
+var air = 0;
+var monk = 0;
+var rich = 0;
+
+if(name) {
+    birdFeed++;
+    popStar++;
+    drugLord++;
+    donDraper++;
+    air++;
+    monk++;
+    rich++;   
+}
+
+if(age < 18) {
+    popStar++;
+    drugLord++;
+    air++;
+    birdFeed++;
+} 
+
+
+if(age > 18) {
+    donDraper++;
+    monk++;
+    rich++;
+}
+
+// Radio List and Check Boxes
+
+var radioList = document.quiz1['favAnimal'];
+for (var i = 0; i < radioList.length; i++){
+    if(radioList[i].checked){
+        var currentValue1 = (radioList[i].value);
+        
+        if(currentValue1 =='cat'){
+        popStar++;
+        air++;
+        }
+          if(currentValue1 == 'dog'){
+              birdFeed++;
+              rich++;       
+        }
+          if(currentValue1 == 'snake'){
+            drugLord++;
+            air++;
+            
+        }
+          if(currentValue1 == 'pig'){
+            birdFeed++;
+            air++;
+            rich++;
+            
+        }
+          if(currentValue1 == 'sloth'){
+            monk++;
+            air++;
+           
+        }
+          if(currentValue1 == 'hamster'){
+            donDraper++;
+            air++;
+            popStar++;
+            
+        }
+          if(currentValue1 == 'me'){
+            air++;
+            rich++;
+            drugLord++;
+            monk++;     
+        }
+    }
+}
+
+var checkBoxes = document.quiz1['important'];
+for (var i = 0; i < checkBoxes.length; i++){
+    if(checkBoxes[i].checked){
+    var currentValue2 = (checkBoxes[i].value);
+
+      if(currentValue2 == 'money'){
+          rich++;
+          drugLord++;
+          air++; 
+    }
+
+      if(currentValue2 == 'family'){
+        drugLord++;
+        donDraper++; 
+    }
+      if(currentValue2 == 'career'){
+        donDraper++;
+        popStar++;
+        monk++;
+        
+    }
+      if(currentValue2 == 'wellBeing'){
+        monk++;
+        birdFeed++;
+        air++;
+       
+    }
+      if(currentValue2 == 'earth'){
+        air++;
+        birdFeed++;
+        monk++;
+        
+    }
+      
+
+    }
+}
+
+
+var highestScore = Math.max(popStar,birdFeed,drugLord,donDraper,air,monk,rich);
+event.preventDefault();
+
+if(highestScore === popStar && highestScore > 0) {
+    document.getElementById('result').innerHTML = "You will be a Popstar!";
+
+} else if (highestScore === donDraper && highestScore > 0) {
+    document.getElementById('result').innerHTML = "You will be Don Draper!";
+
+} else if (highestScore === birdFeed && highestScore > 0) {
+    document.getElementById('result').innerHTML = "You will be bird feed!";
+
+} else if(highestScore === drugLord && highestScore > 0) {
+    document.getElementById('result').innerHTML = "You will be a drug lord!";
+
+} else if(highestScore === air && highestScore > 0){
+    document.getElementById('result').innerHTML = "Air, just air...";
+
+} else if (highestScore === monk && highestScore > 0) {
+    document.getElementById('result').innerHTML = "You will be a Monk!";
+
+} else if (highestScore === rich && highestScore > 0) {
+    document.getElementById('result').innerHTML = "Baby, rich!";
+} 
+
+
 })
 
 
 
 // Scoring 0/10
-// Bird feed
+// Bird feed 
 // A pop star
 // a drug lord
 // Don Draper
