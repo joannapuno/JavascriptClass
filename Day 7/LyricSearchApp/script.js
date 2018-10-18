5fa79b388fd9a9d4bdcfc25323c0dc4c6780afc7
Joanna <j.puno0909@gmail.com>
2018-10-17 20:08:38 -0600

    Lyrics

--- Day 7/LyricSearchApp/script.js
+++ Day 7/LyricSearchApp/script.js
@@ -2,6 +2,7 @@
 var artistName = document.getElementById('searchArtist');
 var songName = document.getElementById('searchSong');
 var submitBtn = document.getElementById('submit');
+var errorMessage = document.getElementById('error');
 
 var lyrics = {
     song: "",
@@ -12,6 +13,7 @@ var lyrics = {
 
 submitBtn.addEventListener('click',function(event){
         event.preventDefault();
+        validateForm();
 
         if(artistName){
             lyrics.artist = artistName.value;
@@ -29,6 +31,31 @@ fetch('https://api.lyrics.ovh/v1/' + artistName.value +"/" +songName.value +"'")
     var newLyrics = data.lyrics;
     newLyrics = newLyrics.replace(/(?:\r\n|\r|\n)/g, '<br>');
     document.getElementById('lyrics').innerHTML = '"' + newLyrics + '"';
+    
+    
 })
 
 });
+
+
+// Validation
+
+function validateForm() {
+    var invalid = false;
+    $('form input').each(function(){
+      if($(this).val()=== '') {
+        invalid = true;
+      } 
+    });
+
+    if(invalid) {
+      $('#error').fadeIn('2000');
+    }
+
+    if(!invalid) {
+      $('#error').hide('2000');
+      
+
+    }
+  }
+
