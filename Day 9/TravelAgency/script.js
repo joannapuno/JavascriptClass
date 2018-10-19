bf8f3eab510b5ff5f101bda1243340131d7318e1
Joanna <j.puno0909@gmail.com>
2018-10-19 13:37:49 -0600

    TravelAgencyWebsite-FinalPush

--- Day 9/TravelAgency/script.js
+++ Day 9/TravelAgency/script.js
@@ -8,6 +8,7 @@
 
 // Mobile Menu
 
+
 var button = document.querySelector('#hamburger-button')
 var menu = document.querySelector('#menu')
 var closeButton = document.querySelector('#close-button')
@@ -25,23 +26,58 @@ button.addEventListener('click', openMenu)
 closeButton.addEventListener('click', closeMenu)
 
 
-
 // Footer Current Date
 let currentDate = new Date();
 $('#currentDate').html(currentDate);
 
+
+// Form Validations
+
+$(document).ready(function() {
+    $('#error').hide();
+    $('#registerBtn').on('click',function(event){
+        event.preventDefault();
+        console.log('hey');
+
+        var invalid = false;
+        $('#user-register input').each(function(){
+    
+            if($(this).val() === '') {
+                invalid = true;
+            }
+        });
+    
+        if(invalid) {
+            $('#error').fadeIn(500);
+            
+        }
+        if(!invalid) {
+            $('#error').hide();
+            
+        }
+      });
+        
+    });
+        
+
+
+// On Submit ---will use PHP
+
+
 // Contact Agents
 const agentNames = ['Jenna Garcia', 'Chloe Smith','Jacob Asper','Jem Puno'];
 const agentPhone = ['403-535-7297','403-555-7357','403-555-5477','403-555-7677'];
+const agentEmail = ['J.Garcia@hop.com','C.Smith@hop.com','J.Asper@hop.com','J.Puno@hop.com'];
 
 $(agentNames).each(function(i) {
     $('h5')[i].append(agentNames[i]);
     $('.agent-box p')[i].append(agentPhone[i]);
+    $('.agent-box span')[i].append(agentEmail[i]);
   });
 
+  
 
-//Package Deal
-
+  // Package Deal
 const packageImages = [
     './img/beach.jpg',
     './img/cruise2.jpg',
@@ -54,22 +90,23 @@ const defaultImg = [
     './img/tour.jpg'
 ];
 
-
 $(packageImages).each(function(i) {
+    
     let img = ($('.products-content img')[i]);
     let info = ($('.products-content')[i]);
         $(info).on({
         mouseenter: function(){
-            
-            $(img).attr('src',packageImages[i]);
+            $(img).fadeOut(function(){
+                $(this).attr('src',packageImages[i]).fadeIn('slow');
+            });  
         
         },
-        mouseleave: function() {
-        
+        mouseleave: function() {  
             $(img).attr('src',defaultImg[i]);
         }
     })
 });
+
 
 
 //Flying plane
@@ -85,6 +122,12 @@ $(document).ready(function() {
     }
     loopPlane();
 });
+
+
+
+
+
+
 
 
 
